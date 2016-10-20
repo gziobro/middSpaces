5140c5e67b96275661a2fc2bb6d4e21c04c66696
Geoffrey Ziobro <gziobro20@gmail.com>
2016-10-19 20:13:45 -0400

    First commit

---
+++ www/js/services.js
@@ -0,0 +1,81 @@
+angular.module('starter.services', [])
+
+.service('checkin', ['$q', '$http', function($q, $http){
+  var api_url= 'https://sheetsu.com/apis/v1.0/73e7d27138f0';
+
+  var currentID = 1;
+
+  var ret={
+    all: function(){
+      var deferred = $q.defer();
+
+      $http.get(api_url).then(function(resp){
+        if (resp.data.length > 0) currentID = parseInt(resp.data[resp.data.length-1].id);
+        deferred.resolve(resp.data);
+      })
+
+      return deferred.promise;
+    }
+    add: function(data){
+      var deferred = $q.defer();
+      currentID ++;
+      data.id=currentID;
+      $http.post(api_url, data).then(function(resp){
+        deferred.resolve(resp.data);
+      });
+      return deferred.promise;
+    }
+  };
+
+  return ret;
+}])
+
+
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'img/ben.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'img/max.png'
+  }, {
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'img/adam.jpg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'img/perry.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'img/mike.png'
+  }];
+
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+});
