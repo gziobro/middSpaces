c3d2b6130ad4fb0247f72b7180477693ae8e93a2
kaysee <kassandra.orozco@gmail.com>
2016-12-03 17:52:41 -0500

    FloorArray modified for new buttons/floors

--- www/js/services.js
+++ www/js/services.js
@@ -6,7 +6,7 @@ angular.module('starter.services', [])
   var currentID = 1;
   var occupied = ""; //stores the occupied space name
   var floorID;
-  var floorArray = [[6,7,8,35],[9,10,11,12,13,14],[28,29,30,31,32,33,34],[15,16,17,18,19,20,21,22],[23,24,25,26,27]];
+  var floorArray = [[55,56,57,58,59,60,61,62,63,64,65,66,67,68,69],[36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54],[6,7,8,35],[9,10,11,12,13,14],[28,29,30,31,32,33,34],[15,16,17,18,19,20,21,22],[23,24,25,26,27]];
   var spaceArray;
   var everything;
 
@@ -90,20 +90,26 @@ angular.module('starter.services', [])
       $http.get(api_url).then(function(resp){
         everything=resp.data;
         console.log(resp.data);
-        if (floorID.id==2){
+        if (floorID.id==0){
           spaceArray=floorArray[0];
-        };
-        if (floorID.id==3){
+        }
+        if (floorID.id==1){
           spaceArray=floorArray[1];
-        };
-        if (floorID.id==4){
+        }
+        if (floorID.id==2){
           spaceArray=floorArray[2];
         };
-        if (floorID.id==5){
+        if (floorID.id==3){
           spaceArray=floorArray[3];
         };
-        if (floorID.id==6){
+        if (floorID.id==4){
           spaceArray=floorArray[4];
+        };
+        if (floorID.id==5){
+          spaceArray=floorArray[5];
+        };
+        if (floorID.id==6){
+          spaceArray=floorArray[6];
         };
 
         for (i=spaceArray[0]-1; i<(spaceArray.length+spaceArray[0]-1); i++){
